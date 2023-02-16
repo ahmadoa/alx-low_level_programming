f94861b6b895294105ee1e7fa2ff2fad9f3d8d14
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-02-16 03:27:06 -0800

    updated puts c file according to betty styling

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void){
+/**
+*main - Entry point function
+*Return: Return 0 if compiling successed and 1 if otherwhise
+*/
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
