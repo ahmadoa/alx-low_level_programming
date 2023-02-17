bdeff5d8a1d22dd99d7d045b4dea41283372adfd
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-02-17 03:13:57 -0800

    updated betty style of 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,12 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if(letter != 'q' &&  letter != 'e')
+		if (letter != 'q' &&  letter != 'e')
 		{
 			putchar(letter);
 		}
 	}
-	
+	putchar('\n');
+
 	return (0);
 }
