a58efbbd71e03b4e4c3e02fef170070b7a2f8ae8
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-02-17 03:34:21 -0800

    added script that print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: returns 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
