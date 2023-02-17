38ad414bba3558c4156e8538d0bde67642b800c2
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-02-17 03:21:54 -0800

    added c script that priint numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: returns 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+
+	return (0);
+}
