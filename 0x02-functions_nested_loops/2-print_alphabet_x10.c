e197493460c13d1e50b8ca605f3e5390f8af6eb5
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-02-21 05:01:07 -0800

    corrected script

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,13 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	int j;
-	char str[] = "_putchar";
-	int len_in_bytes = sizeof(str);
-	int length = len_in_bytes / sizeof(str[0]);
+	char j;
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j <= length - 2; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar(str[j]);
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
