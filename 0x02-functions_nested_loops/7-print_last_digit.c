310dcbb52bd6138fe1b66a52e26181ecb05cf6c7
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-02-21 06:08:10 -0800

    updated 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,16 @@
 int print_last_digit(int n)
 {
 	int ld = n % 10;
+	int c;
 
 	if (ld < 0)
 	{
-		ld += -1;
+		c = -ld;
+		_putchar(c + '0');
 	}
-
-	_putchar(ld + '0');
+	else
+	{
+		_putchar(ld + '0');
+	}
 	return (0);
 }
