63628a559a58652dcd645b171646badf036036b4
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-02-21 08:30:04 -0800

    added 100-times-table.c that printf n times table with conditions

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the 9 times table
+ * @n: how many tables are to be print
+ */
+
+void print_times_table(int n)
+{
+	int num1, num2, prod;
+	int n = 12;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (num1 = 0; num1 <= n; num1++)
+		{
+			printf("0");
+			for (num2 = 1; num2 <= 9; num2++)
+			{
+				prod = num1 * num2;
+
+				if (prod <= 9)
+				{
+					printf(",   %d", prod);
+				}
+				else if (prod >= 100)
+				{
+					printf(", %d", prod);
+				}
+				else if (prod >= 10 && prod <= 99)
+				{
+					printf(",  %d", prod);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
