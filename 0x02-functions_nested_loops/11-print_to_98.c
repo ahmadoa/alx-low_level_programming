d72900660f649f59ab3749c5bd86874d4a768e73
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-02-21 07:57:09 -0800

    updated 11-print_to_98.c betty style

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,11 +23,11 @@ void print_to_98(int n)
 	else
 	{
 		for (i = n; i <= 98; i++)
-                {
+		{
 			if (i == 98)
 				printf("%d\n", i);
 			else
 				printf("%d, ", i);
-                }
+		}
 	}
 }
