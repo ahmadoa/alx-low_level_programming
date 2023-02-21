3cb54618889b46bb690246e9379f51591061dac3
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-02-21 06:54:36 -0800

    added 8-24_hours.c that print every min of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void jack_bauer(void)
+{
+	int min;
+	int hours;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
