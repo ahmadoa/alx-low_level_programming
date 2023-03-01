53df53e25e38ab20d8e34e4065b8bb46e91fba87
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-03-01 13:26:21 -0800

    added 6-cap_string.c which capitalizes each word of a string with conditions

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @p: input string of words
+ * Return: string after conversion
+ */
+
+char *cap_string(char *p)
+{
+	int i;
+
+	i = 0;
+	while (p[i] != '\0')
+	{
+		if (p[i] == 32 || p[i] == 46 || p[i] == '\n' || p[i] == '\t')
+		{
+			if (p[i + 1] >= 'a' && p[i + 1] <= 'z')
+				p[i + 1] = p[i + 1] - 32;
+		}
+		i++;
+	}
+	return (p);
+}
