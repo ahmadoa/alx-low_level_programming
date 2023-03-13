03e8e525f5946ac6e73c6b46de96e7a23a214cb8
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-03-13 01:39:29 -0700

    updated 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,7 @@ char *str_concat(char *s1, char *s2)
 {
 	int s1size = 0;
 	int s2size = 0;
+	int k, limit;
 	char *str;
 
 	while (s1[s1size] != '\0')
@@ -29,23 +30,17 @@ char *str_concat(char *s1, char *s2)
 	str = malloc(sizeof(char) * (s1size + s2size + 1));
 
 	if (str == NULL)
+	{
+		free(str);
 		return (NULL);
-
-	s1size = s2size = 0;
-
-	while (s1[s1size] != '\0')
-	{
-		str[s1size] = s1[s1size];
-		s1size++;
 	}
 
-	while (s2[s2size] != '\0')
-	{
-		str[s1size] = s2[s2size];
-		s1size++;
-		s2size++;
-	}
+	for (k = 0; k < s1size; k++)
+		str[k] = s1[k];
 
-	str[s1size] = '\0';
+	limit = s2size;
+	for (s2size = 0; s2size <= limit; k++; j++)
+		str[k] = s2[s2size];
+
 	return (str);
 }
