51c4d5a3b93fc7955104a0fedcc38f3ee675c760
Ahmad ouladaouid <ouladaouid@gmail.com>
2023-03-12 23:23:23 -0700

    added 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - returns new string containing str
+ * @str: string to copy
+ * Return: new string, NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+	char *newstr;
+	int i, size = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[size] != '\0')
+		size++;
+
+	newstr = malloc(sizeof(char) * (size + 1));
+
+	for (i = 0; i <= size; i++)
+		newstr[i] = str[i];
+
+	return (newstr);
+}
