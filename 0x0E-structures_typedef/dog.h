7a4dc51ac9e19c6472a016a4ae9d19d3a19c93b0
Ahmad oa <ouladaouida@gmail.com>
2023-03-20 15:23:00 -0700

    updated documentation in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _dog_h_
 #define _dog_h_
 
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ */
+
+
 typedef struct dog
 {
 	char *name;
