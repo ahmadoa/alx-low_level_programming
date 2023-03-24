cb5d2a2d2b7e0d1e822278429f5fae78bfbe6637
Ahmad oa <ouladaouida@gmail.com>
2023-03-24 15:55:53 -0700

    updated 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,11 +3,11 @@
 
 /**
  * main - prints its own opcodes
- * @argc: number of passed arguments
- * @argv: array of passed arguments
- * Return: 0
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
 	int bytes, i;
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	bytes = atoi(argc[1]);
+	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
 	{
