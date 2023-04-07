30b5548ea13cccb41dc144516002117f2322a9b4
ahmadoa <ouladaouida@gmail.com>
2023-04-07 06:30:36 -0700

    added 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_file - a function that creates and writes into file
+ * @filename: name of file
+ * @text_content: the text to write into the file
+ * Return: -1 if any operation fails, 1 if it successed
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int filedesc;
+	int size = 0;
+	/*rwr a var to check the return of write and if it was successfull*/
+	int rwr;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+
+	while (text_content[size] != '\0')
+		size++;
+
+	filedesc = open(filedesc, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	rwr = write(filedesc, text_content, size);
+
+	if (filedesc == -1 || rwr == -1)
+		return (-1);
+
+	close(filedesc);
+
+	return (1);
+}
