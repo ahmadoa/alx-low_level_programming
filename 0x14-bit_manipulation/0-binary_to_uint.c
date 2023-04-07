73f709749f5c09743462c4cfcd410beea2880855
ahmadoa <ouladaouida@gmail.com>
2023-04-07 02:30:20 -0700

    updated condition in 0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int index = 0;
 	unsigned int digit = 0;
 
+	if (b == NULL)
+		return (0);
 	while (b[index] != '\0')
 	{
 		if ((b[index] != '0' && b[index] != '1') || b[index] == NULL)
