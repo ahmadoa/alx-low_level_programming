25fc6f95f5f4508e219026ac7b0ac851bc0bfbac
ahmadoa <ouladaouida@gmail.com>
2023-04-07 07:19:01 -0700

    updated 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL || text_content == NULL)
 		return (-1);
 
-	filedesc = open(filename, O_APPEND);
+	filedesc = open(filename, O_WRONLY | O_APPEND);
 
 	if (filedesc == -1)
 		return (-1);
