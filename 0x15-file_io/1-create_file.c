20bec70c21117c39f38221063a7a6b1d9e73758b
ahmadoa <ouladaouida@gmail.com>
2023-04-07 06:47:01 -0700

    updated 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,22 +12,25 @@ int create_file(const char *filename, char *text_content)
 	int filedesc;
 	int size = 0;
 	/*rwr a var to check the return of write and if it was successfull*/
-	int rwr;
+	int rwrite;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
-	{
-		while (text_content[size] != '\0')
-			size++;
-	}
+		text_content = "";
+
+	while (text_content[size] != '\0')
+		size++;
 
 	filedesc = open(filedesc, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	rwr = write(filedesc, text_content, size);
+	rwrite = write(filedesc, text_content, size);
 
-	if (filedesc == -1 || rwr == -1)
+	if (filedesc == -1)
+		return (-1);
+
+	if (rwrite == -1)
 		return (-1);
 
 	close(filedesc);
