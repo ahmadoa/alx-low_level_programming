ba813150af93fd04680e08abe91f64b89108c3bd
ahmadoa <ouladaouida@gmail.com>
2023-04-07 06:56:32 -0700

    update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,18 +17,18 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content == NULL)
 		text_content = "";
 
 	while (text_content[size] != '\0')
 		size++;
 
-	filedesc = open(filedesc, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	rwrite = write(filedesc, text_content, size);
+	filedesc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (filedesc == -1)
 		return (-1);
+
+	rwrite = write(filedesc, text_content, size);
 
 	if (rwrite == -1)
 		return (-1);
