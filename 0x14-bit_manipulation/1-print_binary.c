e84da2e4ed62b5573852443c8961c671e9060026
ahmadoa <ouladaouida@gmail.com>
2023-04-08 14:23:09 -0700

    updated 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,37 @@
 #include "main.h"
 
+/**
+ * print_binary - prints the binary format of an int
+ * @n: the int to convert
+ * Return: void
+ */
+
 void print_binary(unsigned long int n)
 {
-	unsigned int bin = 0;
+	int size = sizeof(n) * 8;
+	int i;
+	int currentBit;
+	int mostSign = -1;
 
-	for (int i = n; i >= 0; i--)
+	if (n == 0)
 	{
-		int bit = n | 1;
+		_putchar('0');
+		return;
+	}
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		currentBit = (n >> i) & 1;
+		if (currentBit)
+		{
+			mostSign = i;
+			break;
+		}
+	}
+
+	for (i = mostSign; i >= 0; i--)
+	{
+		currentBit = (n >> i) & 1;
+		_putchar(currentBit ? '1' : '0');
 	}
 }
