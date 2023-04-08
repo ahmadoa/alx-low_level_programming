bc3c59dd0441d64b66b0db88db31530ed29012c6
ahmadoa <ouladaouida@gmail.com>
2023-04-08 13:38:44 -0700

    updated 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,36 +10,32 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0;
 	int isBin = 1;
-	int index = 0;
-	int digit = 0;
+	int size = 0;
+	int i;
+	int bs = 1;
 
 	if (b == NULL)
 		return (0);
-	while (b[index] != '\0')
+
+	while (b[size] != '\0')
 	{
-		if ((b[index] != '0' && b[index] != '1') || b[index] == NULL)
+		if ((b[size] != '0' && b[size] != '1') || b[size] == NULL)
 		{
 			isBin = 0;
 			break;
 		}
-		index++;
+		size++;
 	}
 
 	if (isBin)
 	{
-		index = 0;
-
-		while (b[index] != '\0')
+		for (i = size - 1; i >= 0; i--)
 		{
-			/**
-			 * convert the char to unit
-			 * since binary is base 2
-			 * multiply the current res by 2 and add the digit
-			 * res represents the current position
-			 */
-			digit = b[index] - '0';
-			res = res * 2 + (digit);
-			index++;
+			if (b[i] == '1')
+			{
+				res = res + bs;
+			}
+			bs = bs * 2;
 		}
 	}
 	else
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void print_binary(unsigned long int n)
+{
+	unsigned int bin = 0;
+
+	for (int i = n; i >= 0; i--)
+	{
+		int bit = n | 1;
+	}
+}
