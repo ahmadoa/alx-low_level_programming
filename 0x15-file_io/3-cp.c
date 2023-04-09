697ea6acbcfae6f6bacf27fee049807615d8525f
ahmadoa <ouladaouida@gmail.com>
2023-04-08 17:00:07 -0700

    added 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * ReadERR - error if reading from file fails
