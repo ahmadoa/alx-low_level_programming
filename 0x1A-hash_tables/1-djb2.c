8871cff414121cbfcebeb944973d0882f94639f6
ahmadoa <ouladaouida@gmail.com>
2023-06-08 09:03:22 -0700

    corrected typos

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,7 +6,7 @@
  *
  * Return: hash value
  */
-unsigned long int hash_dbj2(const unsigned char *str)
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash = 5381;
 	int c;
